6a65b34ec2db4b848c638056e050ae0e8b66fc34
ElenaShylionak <99590421+ElenaShylionak@users.noreply.github.com>
2023-03-22 13:41:09 +0300

    database connection and images

--- src/main/java/com/example/buysell/models/Product.java
+++ src/main/java/com/example/buysell/models/Product.java
@@ -6,6 +6,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 //модель которая будет отображаться в базе данных
 @Entity
 @Table(name = "products")
@@ -28,4 +32,20 @@ public class Product {
         @Column(name = "author")
         private String author;
 
+        @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, //один продукт ко многим фото. ALL - при удалении товара - должны удалиться все фото
+                mappedBy = "product")  //ссылаемся, что товар связанный с фото будет связан и записан в таблице images
+        private List<Image> images = new ArrayList<>();
+        private Long previewImageId;
+        private LocalDateTime dateOfCreated; // знать время когда определенный товар был создан
+
+        @PrePersist
+        private void init() {
+                dateOfCreated = LocalDateTime.now();
+        }
+
+        public void addImageToProduct(Image image) {
+                image.setProduct(this);
+                images.add(image);
+        }
 }
+
--- src/main/java/com/example/buysell/repositories/ProductRepository.java
+++ src/main/java/com/example/buysell/repositories/ProductRepository.java
@@ -1,13 +1,9 @@
 package com.example.buysell.repositories;
 
-
 import com.example.buysell.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-
-//При добавлении зависимости Jpa, подлучаем досутп к репозиторию, чтобы не прописывать свои классы и логику с базами данных
-// получаем функционал "коробки" (методы сохранить в базе данных, удалить, просмотреть, просмотреть по id)
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findByTitle(String title);
